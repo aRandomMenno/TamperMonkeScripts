f5a2ae7649c710f9d9c935bcf52c44637300fe4e
Menno Van Zoelen <109163471+aRandomMenno@users.noreply.github.com>
2024-03-03 18:53:06 +0100

    Update scrollbar.js

--- scrollbar.js
+++ scrollbar.js
@@ -2,20 +2,15 @@
 // @name         CSS customizer
 // @namespace    http://tampermonkey.net/
 // @version      0.3
-// @description  With this you can edit all the css on all webpages! This one edits the scrollbar with css.
-// @author       aRandomMenno
+// @description  Put some css in the css var and watch the website change.
+// @author       you
 // @match        *://*/*
 // @grant        none
 // ==/UserScript==
 
 (function() {
     'use strict';\
-    var css = `
-        ::-webkit-scrollbar { width: 16px; height: 16px; }
-        ::-webkit-scrollbar-thumb { background-color: rgb(5, 5, 5); border-radius: 8px; }
-        ::-webkit-scrollbar-thumb:active { background: rgb(10, 0 ,0); }
-        ::-webkit-scrollbar-thumb:hover { background: rgb(10, 10 ,10); }
-    `;
+    var css = `<any css here>`;
 
     var style = document.createElement('style');
     style.type = 'text/css';
